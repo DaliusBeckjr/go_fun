c742610731c5994140097c24fa81be2a9323281b
Dalius Beck <daliusbeckxiv@gmail.com>
2023-09-18 17:32:42 -0700

    Update main.go

--- functions/passing_var/main.go
+++ functions/passing_var/main.go
@@ -5,17 +5,12 @@ import "fmt"
 func main() {
 	sendsSoFar := 430
 	const sendsToAdd = 25
-	incrementSends(sendsSoFar, sendsToAdd)
+	// capturing the return value of sendsSoFar and overwritting it
+	sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
 	fmt.Println("you've sent", sendsSoFar, "messages")
 }
 
 func incrementSends(sendsSoFar, sendsToAdd int) int {
-	// sendsSoFar = sendsSoFar + sendsToAdd
-	sendsSoFar++
+	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar
 }
-
-// Assignment
-// fix the bug to accurately track the number of SMS messages sent
-
-// Alter the incrementSends function so that it returns the result after incrementing the sendsSoFar.
-// Alter main() to capture the return value from incrementSends() and overwrite the previous sendsSoFar value.
